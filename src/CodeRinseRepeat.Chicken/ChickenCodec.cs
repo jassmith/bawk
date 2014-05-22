85435ef59029be9a7dad91fbc6832ab3e95c558a
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2014-05-22 01:28:41 -0400

    Use @lewing's speedy implementation.

--- src/CodeRinseRepeat.Chicken/ChickenCodec.cs
+++ src/CodeRinseRepeat.Chicken/ChickenCodec.cs
@@ -3,55 +3,111 @@ using System.Text;
 
 namespace CodeRinseRepeat.Chicken
 {
-    public class ChickenCodec {
-        const string ChickenBase = "chicken";
+    public class ChickenCodec
+    {
+        static readonly Char [] FeedStock = { 'C', 'H', 'I', 'C', 'K', 'E', 'N', '.' };
+        public static Lazy<byte []> ChickenFeed = new Lazy<byte []> (SpreadFeed);
 
-        static string Encode(char c)
+        static unsafe int Encode (byte* input, int length, byte* output)
         {
-            var @out = new StringBuilder();
+            if (length == 0)
+                return 0;
 
-            var i = 0;
-            for (; i < 7; i++)
-                @out.Append((c & (1 << i)) != 0 ? Char.ToUpper(ChickenBase[i]) : ChickenBase[i]);
+            var inend = input + length;
+            var outptr = output;
+            var inptr = input;
+            var chickens = ChickenFeed.Value;
+            fixed (byte* coop = chickens) {
+                while (inptr < inend) {
+                    var c = *inptr++;
+                    var chick = coop + ( c * 8 );
 
-            // Is the high bit set?
-            if ((c & (1 << i)) == 1)
-                @out.Append('.');
+                    for (int i = 0; i < 7; i++) {
+                        *outptr++ = *( chick + i );
+                    }
 
-            return @out.ToString();
+                    if (*( chick + 7 ) != 0)
+                        *outptr++ = *( chick + 7 );
+
+                    *outptr++ = (byte)' ';
+                }
+            }
+            return (int)( outptr - output );
         }
 
-        static char Decode(string @in)
+        static byte [] SpreadFeed ()
         {
-            int i = 0, c = 0;
-
-            for (; i < @in.Length; i++)
-                if (Char.IsUpper(@in[i]) || @in[i] == '.')
-                    c |= (1 << i);
-
-            return (char) c;
+            var feed = new byte [256 * 8]; // Wouldn't want bounds checking to slow down our encoder
+            unsafe {
+                fixed (byte* feedptr = feed) {
+                    for (int i = 0; i < 256; i++) {
+                        SeedFeed (feedptr + i * 8, (byte)i);
+                    }
+                }
+            }
+            return feed;
         }
 
-        public static string DeChicken (string body)
+        static unsafe void SeedFeed (byte* feed, byte seed)
         {
-            var chickens = body.Split(' ');
-            var @out = new StringBuilder();
-
-            foreach (var bawk in chickens) {
-                if (bawk.Length > 8)
-                    throw new ArgumentException(String.Format("Could not decode {0}.", bawk));
-                @out.Append(Decode(bawk));
+            for (byte i = 0; i < 8; i++) {
+                var mask = (byte)( 1 << i );
+                *feed++ = (byte)( FeedStock [i] + ( ( ( seed & mask ) > 0 ) ? 0 : 0x20 ) );
             }
 
-            return @out.ToString().TrimEnd();
+            if (*( feed - 1 ) != (byte)'.')
+                *( feed - 1 ) = 0;
         }
 
-        public static string Chicken (string body)
+        static unsafe int Decode (byte* input, int length, byte* output)
         {
-            var output = new StringBuilder();
-            foreach (var c in body)
-                output.AppendFormat("{0} ", Encode(c));
-            return output.ToString().TrimEnd();
+            var position = 0;
+            byte saved = 0;
+            var inend = input + length;
+            var outptr = output;
+            var inptr = input;
+
+            while (inptr < inend) {
+                var c = *inptr++;
+
+                if (c == ' ' || position >= 8) {
+                    // Only consume the chicken if it is reasonably well formed
+                    if (position > 6)
+                        *outptr++ = saved;
+
+                    position = 0;
+                    saved = 0;
+                } else {
+                    saved |= (byte)( ( ( c >= 'A' ) && ( c <= 'Z' ) || c == '.' ) ? ( 1 << position ) : 0 );
+                    position++;
+                }
+            }
+
+            // Consume the last chicken if it was reasonably well-formed.
+            if (position > 6)
+                *outptr++ = saved;
+
+            return (int)( outptr - output );
+        }
+
+        public static unsafe string DeChicken (string body)
+        {
+            var @in = Encoding.UTF8.GetBytes (body);
+            var @out = new byte [(int)Math.Ceiling ((double)body.Length / 8)];
+            fixed (byte* @inptr = @in)
+            fixed (byte* @outptr = @out)
+                Decode (@inptr, @in.Length, @outptr);
+            return Encoding.UTF8.GetString (@out).TrimEnd ('\u0000').TrimEnd ();
+        }
+
+        public static unsafe string Chicken (string body)
+        {
+            var @in = Encoding.UTF8.GetBytes (body);
+            var @out = new byte [body.Length * 9];
+            fixed (byte* @inptr = @in)
+            fixed (byte* @outptr = @out)
+                Encode (@inptr, @in.Length, @outptr);
+            return Encoding.UTF8.GetString (@out).TrimEnd ('\u0000').TrimEnd ();
         }
     }
 }
