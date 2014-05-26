d3e1e334919cede2b873fa5cd8e9f0f550b5dc6c
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2014-05-26 00:05:23 -0400

    Add a stopwatch for timing actions.

--- src/CodeRinseRepeat.Chicken/ChickenController.cs
+++ src/CodeRinseRepeat.Chicken/ChickenController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace CodeRinseRepeat.Chicken
 {
+    [Stopwatch]
     public class ChickenController : ApiController
     {
         [Route ("api/chicken/puke")]
---
+++ src/CodeRinseRepeat.Chicken/StopwatchActionFilter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace CodeRinseRepeat.Chicken
+{
+    public class StopwatchAttribute : ActionFilterAttribute
+    {
+        private readonly Stopwatch stopwatch;
+
+        public StopwatchAttribute ()
+        {
+            stopwatch = new Stopwatch ();
+        }
+
+        public override void OnActionExecuting (HttpActionContext actionContext)
+        {
+            stopwatch.Start ();
+        }
+
+        public override void OnActionExecuted (HttpActionExecutedContext actionExecutedContext)
+        {
+            stopwatch.Stop ();
+            actionExecutedContext.Response.Headers.Add ("X-Stopwatch", stopwatch.Elapsed.ToString ());
+            actionExecutedContext.Response.Headers.Add ("X-Stopwatch-Milliseconds", stopwatch.Elapsed.TotalMilliseconds.ToString (CultureInfo.InvariantCulture));
+        }
+    }
+}
